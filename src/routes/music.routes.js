f54871aee7eb7658ffe62eefc52df58af1529f3a
Aditya1149 <hydrogenspectrum12345@gmail.com>
2026-04-01 02:31:28 +0530

    adding multer

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -1,13 +1,17 @@
 import express from "express"
 import musicController from "../controllers/music.controller.js"
+import multer from "multer"
 
 
+const upload = multer({
+  storage: multer.memoryStorage(), // store the file in memory, we will upload it to imagekit. 
 
+})
 
 const router = express.Router() 
 
 
-router.post("/upload", musicController.createMusic) // this route will be used to upload music, only artists can upload music, so we will check for the role of the user in the controller. we will also use multer middleware to handle the file upload, which will add the file information to the request object, which we can access in the controller.
+router.post("/upload", upload.single("music"), musicController.createMusic) // this route will be used to upload music, only artists can upload music, so we will check for the role of the user in the controller. we will also use multer middleware to handle the file upload, which will add the file information to the request object, which we can access in the controller.
 
 
 
